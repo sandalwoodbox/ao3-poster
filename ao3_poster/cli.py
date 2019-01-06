548747e78cdc06785a98995377a7769a7c14f759
Stephen Burrows <stephen.r.burrows@gmail.com>
2019-01-06 14:38:25 -0800

    Working read of spreadsheet data

--- ao3_poster/cli.py
+++ ao3_poster/cli.py
@@ -1,6 +1,15 @@
+from google.auth.exceptions import DefaultCredentialsError
 import click
+
+from .utils.google_sheets import get_sheet_data
 
 
 @click.command()
-def cli():
-    pass
+@click.argument('sheet_id')
+def cli(sheet_id):
+    try:
+        headers, rows = get_sheet_data(sheet_id)
+    except DefaultCredentialsError as exc:
+        raise click.ClickException(str(exc))
+
+    import pdb; pdb.set_trace()
