da5c1ea6a8fdef9c62f12a6294f7865b5ad3b445
sandalwoodbox <boxofsandalwood@gmail.com>
2019-02-10 20:33:56 -0800

    Switched to session-based posting and split google sheet download into a separate command

--- ao3_poster/cli.py
+++ ao3_poster/cli.py
@@ -1,12 +1,11 @@
 import click
+import csv
 from google.auth.exceptions import DefaultCredentialsError
 
 from . import ao3
 from .exceptions import LoginRequired
 from .exceptions import SessionExpired
 from .exceptions import ValidationError
-from .session import load_session_cookies
-from .session import save_session_cookies
 from .utils.google_sheets import get_sheet_data
 
 
@@ -16,57 +15,54 @@ def cli():
 
 
 @cli.command()
-def login():
-    """
-    Start a new ao3 session
-    """
-    username = click.prompt('Username or email')
-    password = click.prompt(
-        'Password',
-        hide_input=True,
-        confirmation_prompt=True,
-    )
-    session_cookies = ao3.login(username, password)
-    if session_cookies is None:
-        click.secho('Login failed. Please try again.', fg='red')
-    else:
-        save_session_cookies(session_cookies)
-        click.secho('Login successful. Session id saved.')
-
-
-@cli.command()
-def logout():
-    """
-    End your ao3 session
-    """
-    session_cookies = load_session_cookies()
-    if session_cookies is not None:
-        ao3.logout(session_cookies)
-        save_session_cookies(None)
-
-
-@cli.command()
 @click.argument('sheet_id')
+@click.argument('outfile', type=click.File('wb'))
 @click.option(
     '--count',
     default=1,
     type=click.IntRange(1, 10),
     help='How many rows [1-10] to import from the sheet',
 )
-def post(sheet_id, count):
+def get_sheet(sheet_id, outfile, count):
+    click.echo('\nDownloading google sheet data...')
     try:
         headers, rows = get_sheet_data(sheet_id, count)
     except DefaultCredentialsError as exc:
         raise click.ClickException(str(exc))
 
+    click.secho('Done', fg='green')
+
+    writer = csv.DictWriter(outfile, fieldnames=headers)
+
     for row in rows[:count]:
+        writer.writerow(row)
+
+
+@cli.command()
+@click.argument(
+    'csv',
+    type=click.File('rb')
+)
+def post(csv):
+    username = click.prompt('Username or email')
+    password = click.prompt(
+        'Password',
+        hide_input=True,
+        confirmation_prompt=True,
+    )
+    session = ao3.login(username, password)
+    if session is None:
+        raise click.ClickException('Login failed. Please try again.')
+    else:
+        click.secho('Login successful.', fg='green')
+
+    reader = csv.DictReader(csv)
+    for row in reader:
         work_title = row.get('Work Title', 'Unknown Work')
         click.echo('Uploading to AO3: {}'.format(work_title))
 
-        session_cookies = load_session_cookies()
-
         try:
-            work_url = ao3.post(session_cookies, row)
+            work_url = ao3.post(session, row)
         except ValidationError as exc:
             click.echo('Validation errors encountered while processing {}: {}'.format(
                 work_title,
@@ -85,3 +81,5 @@ def post(sheet_id, count):
                 '{} uploaded successfully! {}'.format(work_title, work_url),
                 fg='green',
             )
+
+    ao3.logout(session)
