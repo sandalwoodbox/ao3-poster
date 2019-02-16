ed47f3d14a2e0091785232b7ce6dd5ff4c684af0
sandalwoodbox <boxofsandalwood@gmail.com>
2019-02-16 01:15:16 -0800

    Corrected docs version import

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-import ao3_poster
+import ao3_poster.__version__
 
 
 # -- Project information -----------------------------------------------------
